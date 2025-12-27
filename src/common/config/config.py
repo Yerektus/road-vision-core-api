eb8eec21716be85dbffcbe1c739dc15c4dfe6632
Yerdos <qop6261@gmail.com>
2025-12-27 23:28:20 +0500

    feat: add database helper

--- src/common/config/config.py
+++ src/common/config/config.py
@@ -2,12 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_host: str
-    db_port: int
-    db_user: str
-    db_password: str
-    db_name: str
-    db_sslmode: str
+    db_url: str
+    db_echo: bool
 
 
 settings = Settings()
