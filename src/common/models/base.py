c15e3f5d1cfb282d7dbadafc8153967e03d73b2c
Yerdos <qop6261@gmail.com>
2025-12-28 22:57:54 +0500

    feat: add datetime type

--- src/common/models/base.py
+++ src/common/models/base.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -5,3 +7,12 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        insert_default=func.now()
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        insert_default=func.now(),
+        onupdate=func.now()
+    )
