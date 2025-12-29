10352b018ccf234255b9f0fb9be84c3ecfe90217
Yerdos <qop6261@gmail.com>
2025-12-30 01:24:49 +0500

    feat: update user schema

--- src/common/models/user.py
+++ src/common/models/user.py
@@ -1,8 +1,10 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from .base import Base
 
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTableUUID):
@@ -11,3 +13,7 @@ class User(Base, SQLAlchemyBaseUserTableUUID):
     username: Mapped[str] = mapped_column(String(255), unique=True)
     email: Mapped[str] = mapped_column(String(255), unique=True)
     password: Mapped[str] = mapped_column(String(255))
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
