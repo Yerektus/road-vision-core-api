db5d70a9d472eb170dfd49944f49bfe8366bff80
Yerdos <qop6261@gmail.com>
2025-12-31 00:38:33 +0500

    add http bearer dependency

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from src.modules.auth.presenter.auth_controller import auth_router
 from src.modules.users.presenter.users_controller import users_router
 
-app = FastAPI(title="Road Vision Core API")
+from fastapi.security import HTTPBearer
+
+http_bearer = HTTPBearer(auto_error=False)
+
+app = FastAPI(title="Road Vision Core API", dependencies=(http_bearer))
 app.include_router(auth_router)
 app.include_router(users_router)
 
