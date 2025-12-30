76985947533de793cc63aff8ec7380e7e3200ec3
Yerdos <qop6261@gmail.com>
2025-12-30 13:39:06 +0500

    fix: comment code

--- src/modules/auth/manager/auth_manager.py
+++ src/modules/auth/manager/auth_manager.py
@@ -20,16 +20,16 @@ class AuthManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         log.warning("User %s has registered.", user.id)
 
-    async def on_after_forgot_password(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        log.warning(
-            "User %s has forgot their password. Reset token: %s", user.id, token
-        )
+    # async def on_after_forgot_password(
+    #     self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     log.warning(
+    #         "User %s has forgot their password. Reset token: %s", user.id, token
+    #     )
 
-    async def on_after_request_verify(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        log.warning(
-            "Verification requested for user %s. Verification token: %s", user.id, token
-        )
+    # async def on_after_request_verify(
+    #     self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     log.warning(
+    #         "Verification requested for user %s. Verification token: %s", user.id, token
+    #     )
