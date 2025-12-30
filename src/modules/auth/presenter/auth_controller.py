9a306c1bc91078b4c65b81842d818d0b9dd8e404
Yerdos <qop6261@gmail.com>
2025-12-30 21:26:44 +0500

    feat: add register router

--- src/modules/auth/presenter/auth_controller.py
+++ src/modules/auth/presenter/auth_controller.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 from src.common.dependencies.auth.fastapi_users import fastapi_users
 from src.common.dependencies.auth.auth_backend import authentication_backend
+from src.modules.auth.presenter.schemas.user_schema import UserRead, UserCreate
 
 auth_router = APIRouter(prefix="/api/v1/auth", tags=["auth"])
 
 auth_router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend)
+)
+
+auth_router.include_router(
+    router=fastapi_users.get_register_router(UserRead, UserCreate),
 )
