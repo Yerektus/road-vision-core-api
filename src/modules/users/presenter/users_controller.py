8ee10672e863c5b2175f849e010e76351f200fa6
Yerdos <qop6261@gmail.com>
2025-12-31 00:31:00 +0500

    feat: add users route

--- src/modules/users/presenter/users_controller.py
+++ src/modules/users/presenter/users_controller.py
@@ -1,20 +1,19 @@
 from fastapi import APIRouter, Depends
+from src.common.dependencies.auth.fastapi_users import fastapi_users
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.common.database.db_helper import db_helper
 import src.modules.users.domain.users_service as users_service
+from src.modules.auth.presenter.schemas.user_schema import UserRead, UserUpdate
 
 users_router = APIRouter(prefix="/api/v1/users", tags=["users"])
 
+users_router.include_router(
+    router=fastapi_users.get_users_router(UserRead, UserUpdate),
+)
 
-@users_router.get("/")
+
+@users_router.get("")
 async def get_users(
     session: AsyncSession = Depends(db_helper.scoped_session_depedency),
 ):
     return await users_service.get_users(session=session)
-
-
-@users_router.post("/{user_id}")
-async def create_user(
-    user_id: int, session: AsyncSession = Depends(db_helper.scoped_session_depedency)
-):
-    return await users_service.get_user_by_id(session=session, user_id=user_id)
